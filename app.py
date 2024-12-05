40c7be703a4942900c77a6707956852cb0ff8eca
Mathieu Buannic <mathieu.buannic@epita.fr>
2024-12-05 19:41:33 +0100

    super whitelist

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import subprocess
+
 app = Flask(__name__)
 
-
+# Liste des IP autorisées
+AUTHORIZED_IPS = [
+    "10.74.252.195",
+    "10.74.252.210",
+    "10.74.252.216",
+    "10.74.252.199",
+    "10.74.252.203",
+    "10.74.252.212",
+]
 
 @app.route('/')
 def index():
+    # Obtenir l'adresse IP du client
+    client_ip = request.remote_addr
+    if client_ip not in AUTHORIZED_IPS:
+        # Rediriger vers une route d'erreur si l'IP n'est pas autorisée
+        return redirect(url_for('unauthorized'))
     return render_template('index.html')
+
+@app.route('/unauthorized')
+def unauthorized():
+    return redirect('https://youareanidiot.cc/')
 
 if __name__ == '__main__':
     app.run(host='10.74.252.212', debug=True)
